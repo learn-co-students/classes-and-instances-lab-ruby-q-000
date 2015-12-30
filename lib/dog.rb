ae3a5942d604d87fc2c6708fb47e7d2237393306
nicolefederici <nicolefederici@yahoo.com>
2015-12-30 18:21:58 +0000

    nitrous-backup at Wed Dec 30 18:21:57 UTC 2015

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 class Dog
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+    puts "The dog is sitting."
+  end
 end
-  fido = Dog.new
-  snoopy = Dog.new
-  lassie = Dog.new
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
 
