a07d930084d974cfe16ec723cde31a69593dc0a4
anorico <allan.norico@gmail.com>
2016-03-21 13:16:25 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,14 @@
 class Dog
+
+  #bark
+  def bark
+    puts "Woof"
+  end
+  
+  #sit
+  def sit
+    puts "The Dog is sitting"
+  end
 end
 
 fido = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,14 @@
 class Person
+
+  #talk
+  def talk
+    puts "Hello World!"
+  end
+
+  #walk
+  def walk
+    puts "The Person is walking"
+  end
 end
 
 adele_goldberg = Person.new
