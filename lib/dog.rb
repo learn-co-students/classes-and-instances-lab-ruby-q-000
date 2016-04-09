5684d1f4150f428f565bec74e11ff421276c855f
acodemonkey <bnparsons@gmail.com>
2016-04-09 14:40:16 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+
+  def bark
+    puts "WOOF"
+  end
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+
+  def greet
+
+    puts "Hello"
+
+  end
+
+end
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
