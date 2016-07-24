15570ac29fcd9d304dd69aab242bc973358d4d96
Keith Carrillo <kcee403@gmail.com>
2016-07-24 17:02:48 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+	fido = 0
+	snoopy = 0
+	lassie = 0
+	
+end
+	fido = Dog.new
+	snoopy = Dog.new
+	lassie = Dog.new
+	
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person 
+	adele_goldberg = 0
+	alan_kay = 0
+	
+	end 
+	
+	adele_goldberg = Person.new
+	alan_kay = Person.new
+	
